341fa9c300f976ad13edee2507966029dee32dcc
mahesh313 <jmahesh313@gmail.com>
2017-07-18 16:19:01 +0530

    trying shortcuts in Intellij

---
+++ src/Shortcuts.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jagtapm on 7/18/2017.
+ */
+public class Shortcuts {
+    int a = 56;
+    int b = 65;
+    void show() {
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
+
